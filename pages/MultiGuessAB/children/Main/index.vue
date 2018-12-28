37ad6147a55456cea77614c609b0eb5f9dd6156e
DezChuang <dissaivent@gmail.com>
2018-12-28 10:58:08 +0800

    初始化 @nuxtjs/router 及多人連線對戰房間頁面

---
+++ pages/MultiGuessAB/children/Main/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="pg_multi_main_guessing_number">
+    <h1>決鬥吧！遊戲 BOY！</h1>
+    <Button>創建新房間</Button>
+    <Button>加入房間</Button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Main',
+    head: {
+      title: '1A2B Guessing Number'
+    },
+    data() {
+      return {}
+    },
+    methods: {}
+  }
+</script>
+
+<style lang="scss" src="./style.scss" scoped></style>
